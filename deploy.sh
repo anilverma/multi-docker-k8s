d361f1cd4d9b5fd4490ba0a7163128cdc8448f6f
Anil <leo.anil@gmail.com>
2021-02-01 18:02:38 +0530

    cert and issuer

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t anilverma/multi-client:latest  -t anilverma/multi-client:$SHA -f ./frontend/Dockerfile ./frontend
 docker build -t anilverma/multi-server:latest -t anilverma/multi-server:$SHA  -f ./server/Dockerfile ./server
-docker build -t anilverma/multi-worker -t anilverma/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t anilverma/multi-worker:latest -t anilverma/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push anilverma/multi-client:latest 
 docker push anilverma/multi-server:latest
